b32aa09f0378b4be96248e7283c4bb17042cf5f6
Fabian David Romero <fabian.romero.jimenez@gmail.com>
2019-07-17 22:34:44 -0500

    fromero: cambio favicon

--- ToDoApp/src/app/components/main-frame/main-frame.module.ts
+++ ToDoApp/src/app/components/main-frame/main-frame.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { AppNavBar } from './nav-bar/nav-bar.component';
 import { AppFrame } from './app-frame/app-frame.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppMenu } from './app-menu/app-menu.component';
 
 
 @NgModule({
     declarations: [
-        AppNavBar, AppFrame
+        AppNavBar, AppFrame, AppMenu
     ],
     imports: [
         AppRoutingModule
